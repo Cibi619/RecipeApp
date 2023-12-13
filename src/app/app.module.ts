cccdb8fb7a2c14a81ff8d9905feba94768e4cfd6
Cibi619 <cibi.mit@gmail.com>
2023-12-13 12:30:31 -0600

    Project Components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './shopping-components/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './shopping-components/shopping-list-edit/shopping-list-edit.component';
+import { IngredientsComponent } from './shopping-components/ingredients/ingredients.component';
+import { RecipesComponent } from './recipes/recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    IngredientsComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent
   ],
   imports: [
     BrowserModule,
